8457b11117cb24de666d9ea53f05785a7c8a5e1b
Alfonso Gonzalez <alfonso_gonzalezg@outlook.com>
2023-05-10 16:30:02 -0500

    Borrando código

--- src/App.tsx
+++ src/App.tsx
@@ -10,65 +10,6 @@ import { CreateToDoButton } from './components/CreateToDoButton/CreateToDoButton
 import './App.css';
 
 function App(): JSX.Element {
-  // // const {toDoList, saveToDoList, loading, error} = useLocalStorage('toDoList_V1', []);
-  // const [searchTask, setSearchTask] = useState<string>("");
-  // const [toDoList, setToDoList] = useState<ITask[]>([]);
-  // const [loading, setLoading] = useState<boolean>(true);
-  // const [error, setError] = useState<boolean>(false);
-
-  // const completedToDoList = toDoList.filter(toDo => toDo.completed).length;
-  // const totalToDoList = toDoList.length;
-
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     try {
-  //       const localStorageToDo: (string | null) = localStorage.getItem('toDoList_V1');
-  //       let parsedToDoList: ITask[] = localStorageToDo ? JSON.parse(localStorageToDo) : [];
-
-  //       if (!localStorageToDo) localStorage.setItem('toDoList_V1', JSON.stringify([]));
-
-  //       setToDoList(parsedToDoList);
-  //       setLoading(false);
-  //     } catch (error) {
-  //       setError(true);
-  //     }
-  //   }, 1000);
-  // }, [totalToDoList]);
-
-  // const onCreateTask = (value: string) => {
-  //   console.log(value);
-  // };
-
-  // const filterTasks = (item: ITask): boolean => {
-  //   return (item.text).toLowerCase().includes(searchTask.toLowerCase());
-  // };
-
-  // const saveToDoList = (newTodoList: ITask[]): void => {
-  //   try {
-  //     localStorage.setItem('toDoList_V1', JSON.stringify(newTodoList));
-  //     setToDoList(newTodoList);
-  //   } catch(error) {
-  //     setError(true);
-  //   }
-  // };
-
-  // const onComplete = (id: number) => {
-  //   let newTodoList = toDoList.map(toDo => {
-  //     if (toDo.id === id) {
-  //       return { ...toDo, completed: toDo.completed ? false : true }
-  //     } else {
-  //       return toDo
-  //     }
-  //   });
-
-  //   saveToDoList(newTodoList);
-  // };
-
-  // const onDelete = (id: number) => {
-  //   let deleteFromTodoList = toDoList.filter(toDo => toDo.id !== id);
-  //   saveToDoList(deleteFromTodoList);
-  // };
-
   const { error, loading, toDoList, setSearchTask, filterTasks, onComplete, onDelete, setCreateTaskText, onCreateTask, createTaskText } = useContext(ToDoContext);
 
   return (
